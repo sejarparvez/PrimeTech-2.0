795eb198d5682da8c9766d8ad3ed0f450842b19f
sejarparvez <sejarparvez@yahoo.com>
2024-01-02 09:06:59 +0600

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/layout/Footer";
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import Navbar from "@/components/layout/Navbar";
 import RightSidebar from "@/components/layout/rightsidebar/RightSidebar";
@@ -6,22 +7,25 @@ import RecentPost from "@/components/pages/home/RecentPost";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-20">
-        <LeftSidebar />
-      </div>
-      <div className="w-full border-l">
-        <Navbar />
-        <div className="grid-cols-48  grid">
-          <div className="col-span-37 border-r px-4 pt-4">
-            <Featured />
-            <RecentPost />
-          </div>
-          <div className="col-span-11">
-            <RightSidebar />
+    <div>
+      <div className="flex">
+        <div className="w-20">
+          <LeftSidebar />
+        </div>
+        <div className="w-full border-l">
+          <Navbar />
+          <div className="grid  grid-cols-48">
+            <div className="col-span-37 border-r px-4 pt-4">
+              <Featured />
+              <RecentPost />
+            </div>
+            <div className="col-span-11">
+              <RightSidebar />
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
