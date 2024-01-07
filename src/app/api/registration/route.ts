a5433a983ff1f5c3a7836fe190611228a1f801e6
sejarparvez <sejarparvez@yahoo.com>
2024-01-07 08:56:16 +0600

    added smtp server

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 const prisma = new PrismaClient();
 
@@ -15,19 +16,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
       });
     }
 
-    try {
-      const existingUser = await prisma.user.findFirst({
-        where: {
-          email: email,
-        },
-      });
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-      if (existingUser) {
-        return new NextResponse("Email is already registered", { status: 409 });
-      }
-    } catch (error) {
-      console.error("Database error:", error);
-      return new NextResponse("Database error", { status: 500 });
+    if (existingUser) {
+      return new NextResponse("Email is already registered", { status: 409 });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -41,11 +37,50 @@ export async function POST(req: NextRequest, res: NextResponse) {
       },
     });
 
-    return NextResponse.json(user, { status: 201 });
+    const verificationCode = generateVerificationCode();
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_SERVER,
+      port: parseInt(process.env.SMTP_PORT || "587"),
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    if (!email) {
+      return new NextResponse("Fill out the required fields", { status: 400 });
+    }
+
+    const mailData = {
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: "Password Reset Verification Code",
+      text: `Your verification code for password reset: ${verificationCode} | Sent from: PrimeTech`,
+      html: `<p>Your verification code for creating new user: <strong>${verificationCode}</strong></p><p>Sent from: PrimeTech</p>`,
+    };
+
+    // Save the verification code in the database
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { verificationCode: verificationCode },
+    });
+
+    const info = await transporter.sendMail(mailData);
+
+    console.log("Message sent successfully", info);
+
+    return new NextResponse("Message sent successfully", { status: 200 });
   } catch (error) {
-    console.error("Internal server error:", error);
+    console.error("Error:", error);
     return new NextResponse("Internal server error", { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
+}
+
+function generateVerificationCode() {
+  // Generate a random 6-digit code
+  return Math.floor(100000 + Math.random() * 900000).toString();
 }
