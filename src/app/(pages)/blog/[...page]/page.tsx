a1ad6a25a55d8361e2e27c9cecedf297269739b0
sejarparvez <sejarparvez@yahoo.com>
2024-01-27 16:37:51 +0600

    added blog page

---
+++ src/app/(pages)/blog/[...page]/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import PaginationUi from "@/components/common/pagination/Pagination";
+import { FetchAllPost } from "@/components/fetch/get/allpost/FetchAllPost";
+import Loading from "@/components/helper/Loading";
+import RecentPostModel from "@/components/pages/home/RecentPostModel";
+import FeaturedPostType from "@/components/type/post/FeaturedPostType";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Blog() {
+  const params = useParams<{ page: string }>();
+  const { data, isLoading, isError } = FetchAllPost();
+
+  const [page, setPage] = useState<number>(Number(params.page[1]) || 1);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <p>Error loading posts. Please try again later.</p>;
+  }
+
+  const postCount = data.totalPostsCount;
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
+        {data.posts.map((post: FeaturedPostType) => (
+          <RecentPostModel
+            key={uuidv4()}
+            title={post.title}
+            image={post.coverImage}
+            authorImage={post.author.image}
+            updatedAt={post.updatedAt}
+            comments={post._count.comments}
+            category={post.category}
+            createdAt={post.createdAt}
+            content={post.content}
+          />
+        ))}
+      </div>
+      <div className="my-20">
+        <PaginationUi
+          currentPage={page}
+          totalPages={Math.ceil(Number(postCount) / 10)}
+          setCurrentPage={(newPage) => setPage(newPage)}
+        />
+      </div>
+    </>
+  );
+}
--- src/components/fetch/get/allpost/FetchAllPost.tsx
+++ src/components/fetch/get/allpost/FetchAllPost.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export function FetchAllPost() {
+
   return useQuery({
     queryKey: ["allPosts"],
     queryFn: async () => {
