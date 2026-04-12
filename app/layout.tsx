9f50c806d001e21735d8433fd4aa49a8d3e501cf
Sejar Parvez <sejarparvez@yahoo.com>
2026-04-12 18:27:22 +0600

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Geist } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { TanstackProvider } from '../provider/QueryProvider';
 import { ThemeProvider } from '../provider/ThemeProvider';
 import './globals.css';
+import { cn } from "@/lib/utils";
+
+const geistHeading = Geist({subsets:['latin'],variable:'--font-heading'});
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning className={cn(geistHeading.variable)}>
       <body className={inter.className}>
         <TanstackProvider>
           <ThemeProvider
