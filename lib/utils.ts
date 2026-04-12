be9e001b6cdafb31887864960ac2a9fd2ed68aa5
Sejar Parvez <sejarparvez@yahoo.com>
2026-04-12 18:23:06 +0600

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,16 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-export function getInitials(name?: string | null): string {
-  if (!name) {
-    return '';
-  }
-  const words = name.trim().split(' ');
-  if (words.length === 1) {
-    return words[0][0].toUpperCase();
-  } // Handles single-word names
-  return `${words[0][0].toUpperCase()}${words[words.length - 1][0].toUpperCase()}`; // Handles multi-word names
+  return twMerge(clsx(inputs))
 }
