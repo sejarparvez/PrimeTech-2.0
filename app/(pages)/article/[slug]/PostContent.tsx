bc353948a9e91c8e4ab3564ba003068eb9177edb
Sejar Parvez <sejarparvez@yahoo.com>
2026-04-17 07:57:00 +0600

    improved tiptap renderer

--- app/(pages)/article/[slug]/PostContent.tsx
+++ app/(pages)/article/[slug]/PostContent.tsx
@@ -20,9 +20,13 @@ const PostContent = ({ children }: PostContentProps) => {
           font-weight: var(--shiki-dark-font-weight) !important;
           text-decoration: var(--shiki-dark-text-decoration) !important;
         }
+          .article-content :is(p, li, blockquote, td) a {
+          color: var(--primary);
+          }
       `}</style>
 
       <div className='article-content prose prose-neutral order-2 min-w-full dark:prose-invert prose-headings:scroll-m-20 [&_li_p]:m-0! [&_pre]:relative [&_pre]:overflow-x-auto [&_pre]:bg-[hsl(224,19%,97%)] [&_pre]:py-4 dark:[&_pre]:bg-[hsl(224,14%,16%)] [&_pre_code]:block [&_pre_code]:w-fit [&_pre_code]:min-w-full [&_pre_code]:px-5 [&_pre_code]:text-sm [&_pre_code]:leading-7'>
+        {' '}
         {children}
       </div>
     </>
--- tiptap-renderer/components/heading-with-anchor.tsx
+++ tiptap-renderer/components/heading-with-anchor.tsx
@@ -32,7 +32,7 @@ const HeadingWithAnchor: React.FC<HeadingWithAnchorProps> = ({
       <Link
         href={`#${headingId}`}
         aria-label={`Link to ${headingId}`}
-        className="not-prose font-inherit group relative hover:before:absolute hover:before:-left-6 hover:before:top-1/2 hover:before:-translate-y-1/2 hover:before:text-[1em] hover:before:opacity-70 hover:before:content-['#']"
+        className="not-prose font-inherit text-inherit group relative hover:before:absolute hover:before:-left-6 hover:before:top-1/2 hover:before:-translate-y-1/2 hover:before:text-[1em] hover:before:opacity-70 hover:before:content-['#']"
       >
         {children}
       </Link>
